ed034309da6ea57c8bc45e351ad5593024176cdc
AFitting <asf45@humboldt.edu>
2020-02-13 10:15:00 -0800

    Vid 4 - 1:38.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -10,5 +10,7 @@ public class TimelineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        TwitterApp.getRestClient(this);
     }
 }
