29735760dc0cf58331c08df9d8cd329543b6d931
AFitting <asf45@humboldt.edu>
2020-02-16 11:26:05 -0800

    Vid 5

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,10 @@
+package com.codepath.apps.restclienttemplate;
+
+public class TweetsAdapter {
+
+    //Pass in the context and list of tweets
+
+    //For each row, inflate the layout
+
+    //Bind values based on the position
+}
